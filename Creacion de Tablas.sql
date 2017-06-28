32368f6a4998b21627f4ed3ecc077c92343ffb47
Pablo Madrigal <pjmq2@hotmail.com>
2017-06-27 20:59:07 -0600

    Base de datos para Terapeuta Fisica

--- Creacion de Tablas.sql
+++ Creacion de Tablas.sql
@@ -2,20 +2,23 @@ Use DB_GRUPO3
 
 --Tabla Estudiante
 create table PACIENTE(
-	Cedula		char(11)	not null primary key,
-	NombreP		varchar(15)	not null,
-	Apellido1	varchar(15)	not null,
+	Cedula		char(11)		not null primary key,
+	NombreP		varchar(15)		not null,
+	Apellido1	varchar(15)		not null,
 	Apellido2	varchar(15),
-	Edad		int not null,
-	Sexo		char(1)		not null,
+	Edad		int				not null,
+	Sexo		char(1)			not null,
+	Valoracion	varchar(500)	not null,
 	Email		varchar(50),
-	Numero		int
+	Numero		int,
+	Comentarios	varchar(500),
 )
 
 --Tabla Cirugias
 create table CIRUGIAS(
 	CedPaciente	char(11)	not null,
 	Cirugia		varchar(50)	not null,
+	Objetivos	varchar(500) not null,
 	primary key (CedPaciente, Cirugia),
 	foreign key	(CedPaciente) references PACIENTE(Cedula)
 		on delete no action
@@ -38,9 +41,7 @@ create table PLAN_EJERCICIOS(
 	Nivel		varchar(10) not null,
 	Objetivos	varchar(500),
 	primary key (CedPaciente, Padec_Act, Nivel),
-	foreign key (CedPaciente) references PACIENTE(Cedula)
-		on delete no action,
-	foreign key (Padec_Act) references PLAN_TRATAMIENTO(Pad_Actual)
+	foreign key (CedPaciente,Padec_Act) references PLAN_TRATAMIENTO(CedPaciente,Pad_Actual)
 		on delete no action
 )
 
@@ -55,33 +56,28 @@ create table CITA(
 	Lugar		varchar(50),
 	Estado_Paciente varchar(50),
 	primary key (CedPaciente, Padec_Act, Fecha),
-	foreign key (CedPaciente) references PACIENTE(Cedula)
-		on delete no action,
-	foreign key (Padec_Act) references PLAN_TRATAMIENTO(Pad_Actual)
+	foreign key (CedPaciente,Padec_Act) references PLAN_TRATAMIENTO(CedPaciente,Pad_Actual)
 		on delete no action
 )
 
---Tabla Antecedentes Patologicos Personales
-create table ANT_PAT_PER(
+--Tabla Antecedentes Patologicos
+create table ANT_PAT(
 	Nombre		varchar(50)	not null primary key,
 	Descripcion	varchar(500)
 )
 
---Tabla Antecedentes Patologicos Familiares
-create table ANT_PAT_FAM(
-	Nombre		varchar(50)	not null primary key,
-	Descripcion	varchar(500)
-)
-
---Tabla Fracturas y Traumas
-create table FRACTURAS_TRAUMAS(
-	Nombre		varchar(50)	not null primary key,
-	Descripcion	varchar(500)
+--Tabla Tipo de Antecedentes Patologicos
+create table TIPO_ANT(
+	Nombre_Ant		varchar(50)	not null,
+	Tipo			varchar(50) not null,
+	primary key (Nombre_Ant, Tipo),
+	foreign key (Nombre_Ant) references ANT_PAT(Nombre)
+		on delete no action,
 )
 
 --Tabla Datos Clinicos
 create table DATOS_CLINICOS(
-	CedPaciente	char(11)	not null,
+	CedPaciente	char(11)		not null,
 	Dato		varchar(500)	not null,
 	primary key (CedPaciente, Dato),
 	foreign key (CedPaciente) references PACIENTE(Cedula)
@@ -90,8 +86,9 @@ create table DATOS_CLINICOS(
 
 --Tabla Tecnicas
 create table TECNICAS(
-	Nombre		varchar(50)	not null primary key,
-	Descripcion	varchar(500)	not null
+	Nombre		varchar(50)		not null primary key,
+	Descripcion	varchar(500)	not null,
+	Precio		int
 )
 
 --Tabla Se Realiza
@@ -101,9 +98,9 @@ create table SE_REALIZA(
 	Fecha_Cita	datetime	not null,
 	Nombre_Tec	varchar(50)	not null
 	primary key (CedPaciente, Padec_Act, Fecha_Cita, Nombre_Tec),
-	foreign key (CedPaciente) references PACIENTE(Cedula)
+	foreign key (CedPaciente,Padec_Act,Fecha_Cita) references CITA(CedPaciente, Padec_Act, Fecha)
 		on delete no action,
-	foreign key (Padec_Act) references PLAN_TRATAMIENTO(Pad_Actual)
+	foreign key (Nombre_Tec) references TECNICAS(Nombre)
 		on delete no action
 )
 
@@ -117,6 +114,7 @@ create table MATERIAL(
 create table REQUIERE_DE(
 	Nombre_Tec	varchar(50)	not null,
 	Nombre_Mat	varchar(50) not null ,
+	Cantidad	int,
 	foreign key (Nombre_Tec) references TECNICAS(Nombre)
 		on delete no action,
 	foreign key (Nombre_Mat) references MATERIAL(Nombre)
@@ -130,14 +128,33 @@ create table TAREAS_PLAN_EJERCICIOS(
 	Nivel_Plan	varchar(10)	not null,
 	Tarea		varchar(50)	not null
 	primary key (CedPaciente, Padec_Act, Nivel_Plan, Tarea),
-	foreign key (CedPaciente) references PACIENTE(Cedula)
-		on delete no action,
-	foreign key (Padec_Act) references PLAN_TRATAMIENTO(Pad_Actual)
-		on delete no action,
-	foreign key (Nivel_Plan) references PLAN_EJERCICIOS(Nivel)
+	foreign key (CedPaciente,Padec_Act,Nivel_Plan) references PLAN_EJERCICIOS(CedPaciente,Padec_Act,Nivel)
 		on delete no action
 )
 
+--Tabla Ejercicios
+create table EJERCICIO(
+	Nombre		varchar(50) not null primary key,
+	Descripcion	varchar(500) not null,
+	Imagen		image,
+)
+
+--Tabla Consta de 
+create table CONSTA_DE(
+	CedPaciente	char(11)	not null,
+	Padec_Act	varchar(50)	not null,
+	Nivel_Plan	varchar(10)	not null,
+	Nombre_Eje	varchar(50)	not null,
+	Tiempo		int,
+	Repeticion	int,
+	primary key (CedPaciente, Padec_Act, Nivel_Plan, Nombre_Eje),
+	foreign key (CedPaciente,Padec_Act,Nivel_Plan) references PLAN_EJERCICIOS(CedPaciente,Padec_Act,Nivel)
+		on delete no action,
+	foreign key (Nombre_Eje) references EJERCICIO(Nombre)
+		on delete no action,
+)
+
+/*
 --Tabla Doctor
 create table DOCTOR(
 	Telefono	int			not null primary key,
@@ -170,29 +187,24 @@ create table REFERENCIA(
 	foreign key (CedPaciente) references PACIENTE(Cedula)
 		on delete no action,
 )
+*/
 
---Tabla Ejercicios
-create table EJERCICIO(
-	Nombre		varchar(50) not null primary key,
-	Descripcion	varchar(500) not null,
-	Imagen		image,
-)
 
---Tabla Consta de 
-create table CONSTA_DE(
-	CedPaciente	char(11)	not null,
-	Padec_Act	varchar(50)	not null,
-	Nivel_Plan	varchar(10)	not null,
-	Nombre_Eje	varchar(50)	not null,
-	Tiempo		int,
-	Repeticion	int,
-	primary key (CedPaciente, Padec_Act, Nivel_Plan, Nombre_Eje),
-	foreign key (CedPaciente) references PACIENTE(Cedula)
-		on delete no action,
-	foreign key (Padec_Act) references PLAN_TRATAMIENTO(Pad_Actual)
-		on delete no action,
-	foreign key (Nivel_Plan) references PLAN_EJERCICIOS(Nivel)
-		on delete no action,
-	foreign key (Nombre_Eje) references EJERCICIO(Nombre)
-		on delete no action,
-)
+--Borrar TODO
+/*
+Drop table PACIENTE
+Drop table CIRUGIAS
+Drop table PLAN_TRATAMIENTO
+Drop table PLAN_EJERCICIOS
+Drop table CITA
+Drop table ANT_PAT
+Drop table TIPO_ANT
+Drop table DATOS_CLINICOS
+Drop table TECNICAS
+Drop table SE_REALIZA
+Drop table MATERIAL
+Drop table REQUIERE_DE
+Drop table TAREAS_PLAN_EJERCICIOS
+Drop table EJERCICIO
+Drop table CONSTA_DE
+*/
