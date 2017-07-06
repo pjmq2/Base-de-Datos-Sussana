bc5bd4c5d01217bb744db918814e89798635a740
Pablo Madrigal <pjmq2@hotmail.com>
2017-07-06 01:00:57 -0600

    Cambios en menus

--- BD_Grupo3_VS/BD_Grupo3_VS/ConsultarPaciente.Designer.cs
+++ BD_Grupo3_VS/BD_Grupo3_VS/ConsultarPaciente.Designer.cs
@@ -38,6 +38,7 @@
             this.DGV_Pacientes = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.TXT_FiltroNombre = new System.Windows.Forms.TextBox();
+            this.CB_Nombre = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Pacientes)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,18 +47,20 @@
             this.LBL_ConsultaDePacientes.AutoSize = true;
             this.LBL_ConsultaDePacientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LBL_ConsultaDePacientes.ForeColor = System.Drawing.Color.Maroon;
-            this.LBL_ConsultaDePacientes.Location = new System.Drawing.Point(21, 26);
+            this.LBL_ConsultaDePacientes.Location = new System.Drawing.Point(28, 32);
+            this.LBL_ConsultaDePacientes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LBL_ConsultaDePacientes.Name = "LBL_ConsultaDePacientes";
-            this.LBL_ConsultaDePacientes.Size = new System.Drawing.Size(306, 33);
+            this.LBL_ConsultaDePacientes.Size = new System.Drawing.Size(393, 42);
             this.LBL_ConsultaDePacientes.TabIndex = 3;
             this.LBL_ConsultaDePacientes.Text = "Consulta de Pacientes";
             // 
             // LINK_Menu
             // 
             this.LINK_Menu.AutoSize = true;
-            this.LINK_Menu.Location = new System.Drawing.Point(631, 26);
+            this.LINK_Menu.Location = new System.Drawing.Point(841, 32);
+            this.LINK_Menu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LINK_Menu.Name = "LINK_Menu";
-            this.LINK_Menu.Size = new System.Drawing.Size(97, 13);
+            this.LINK_Menu.Size = new System.Drawing.Size(128, 17);
             this.LINK_Menu.TabIndex = 4;
             this.LINK_Menu.TabStop = true;
             this.LINK_Menu.Text = "Ir al Menú Principal";
@@ -65,9 +68,10 @@
             // LINK_AgregarPaciente
             // 
             this.LINK_AgregarPaciente.AutoSize = true;
-            this.LINK_AgregarPaciente.Location = new System.Drawing.Point(631, 46);
+            this.LINK_AgregarPaciente.Location = new System.Drawing.Point(841, 57);
+            this.LINK_AgregarPaciente.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LINK_AgregarPaciente.Name = "LINK_AgregarPaciente";
-            this.LINK_AgregarPaciente.Size = new System.Drawing.Size(89, 13);
+            this.LINK_AgregarPaciente.Size = new System.Drawing.Size(118, 17);
             this.LINK_AgregarPaciente.TabIndex = 9;
             this.LINK_AgregarPaciente.TabStop = true;
             this.LINK_AgregarPaciente.Text = "Agregar Paciente";
@@ -76,34 +80,38 @@
             // LBL_FiltroNombre
             // 
             this.LBL_FiltroNombre.AutoSize = true;
-            this.LBL_FiltroNombre.Location = new System.Drawing.Point(46, 121);
+            this.LBL_FiltroNombre.Location = new System.Drawing.Point(61, 149);
+            this.LBL_FiltroNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LBL_FiltroNombre.Name = "LBL_FiltroNombre";
-            this.LBL_FiltroNombre.Size = new System.Drawing.Size(47, 13);
+            this.LBL_FiltroNombre.Size = new System.Drawing.Size(62, 17);
             this.LBL_FiltroNombre.TabIndex = 10;
             this.LBL_FiltroNombre.Text = "Nombre:";
             // 
             // LBL_FiltroGeneral
             // 
             this.LBL_FiltroGeneral.AutoSize = true;
-            this.LBL_FiltroGeneral.Location = new System.Drawing.Point(308, 121);
+            this.LBL_FiltroGeneral.Location = new System.Drawing.Point(411, 149);
+            this.LBL_FiltroGeneral.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LBL_FiltroGeneral.Name = "LBL_FiltroGeneral";
-            this.LBL_FiltroGeneral.Size = new System.Drawing.Size(72, 13);
+            this.LBL_FiltroGeneral.Size = new System.Drawing.Size(98, 17);
             this.LBL_FiltroGeneral.TabIndex = 12;
             this.LBL_FiltroGeneral.Text = "Filtro General:";
             // 
             // TXT_FiltroGeneral
             // 
             this.TXT_FiltroGeneral.BackColor = System.Drawing.SystemColors.Window;
-            this.TXT_FiltroGeneral.Location = new System.Drawing.Point(406, 119);
+            this.TXT_FiltroGeneral.Location = new System.Drawing.Point(541, 146);
+            this.TXT_FiltroGeneral.Margin = new System.Windows.Forms.Padding(4);
             this.TXT_FiltroGeneral.Name = "TXT_FiltroGeneral";
-            this.TXT_FiltroGeneral.Size = new System.Drawing.Size(146, 20);
+            this.TXT_FiltroGeneral.Size = new System.Drawing.Size(193, 22);
             this.TXT_FiltroGeneral.TabIndex = 13;
             // 
             // BTN_Buscar
             // 
-            this.BTN_Buscar.Location = new System.Drawing.Point(627, 113);
+            this.BTN_Buscar.Location = new System.Drawing.Point(836, 139);
+            this.BTN_Buscar.Margin = new System.Windows.Forms.Padding(4);
             this.BTN_Buscar.Name = "BTN_Buscar";
-            this.BTN_Buscar.Size = new System.Drawing.Size(93, 30);
+            this.BTN_Buscar.Size = new System.Drawing.Size(124, 37);
             this.BTN_Buscar.TabIndex = 15;
             this.BTN_Buscar.Text = "Buscar";
             this.BTN_Buscar.UseVisualStyleBackColor = true;
@@ -111,34 +119,53 @@
             // 
             // DGV_Pacientes
             // 
+            this.DGV_Pacientes.AllowUserToAddRows = false;
+            this.DGV_Pacientes.AllowUserToDeleteRows = false;
             this.DGV_Pacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_Pacientes.Location = new System.Drawing.Point(49, 182);
+            this.DGV_Pacientes.Location = new System.Drawing.Point(65, 224);
+            this.DGV_Pacientes.Margin = new System.Windows.Forms.Padding(4);
+            this.DGV_Pacientes.MultiSelect = false;
             this.DGV_Pacientes.Name = "DGV_Pacientes";
-            this.DGV_Pacientes.Size = new System.Drawing.Size(671, 321);
+            this.DGV_Pacientes.ReadOnly = true;
+            this.DGV_Pacientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGV_Pacientes.Size = new System.Drawing.Size(895, 395);
             this.DGV_Pacientes.TabIndex = 16;
+            this.DGV_Pacientes.CurrentCellChanged += new System.EventHandler(this.DGV_Pacientes_CurrentCellChanged_1);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 80);
+            this.label1.Location = new System.Drawing.Point(32, 98);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(247, 13);
+            this.label1.Size = new System.Drawing.Size(334, 17);
             this.label1.TabIndex = 17;
             this.label1.Text = "En el filtro general puede ingresar apellido o cédula";
             // 
             // TXT_FiltroNombre
             // 
             this.TXT_FiltroNombre.BackColor = System.Drawing.SystemColors.Window;
-            this.TXT_FiltroNombre.Location = new System.Drawing.Point(112, 118);
+            this.TXT_FiltroNombre.Location = new System.Drawing.Point(149, 145);
+            this.TXT_FiltroNombre.Margin = new System.Windows.Forms.Padding(4);
             this.TXT_FiltroNombre.Name = "TXT_FiltroNombre";
-            this.TXT_FiltroNombre.Size = new System.Drawing.Size(146, 20);
+            this.TXT_FiltroNombre.Size = new System.Drawing.Size(193, 22);
             this.TXT_FiltroNombre.TabIndex = 18;
+            // 
+            // CB_Nombre
+            // 
+            this.CB_Nombre.FormattingEnabled = true;
+            this.CB_Nombre.Location = new System.Drawing.Point(146, 183);
+            this.CB_Nombre.Name = "CB_Nombre";
+            this.CB_Nombre.Size = new System.Drawing.Size(195, 24);
+            this.CB_Nombre.TabIndex = 19;
+            this.CB_Nombre.SelectedIndexChanged += new System.EventHandler(this.CB_Nombre_SelectedIndexChanged);
             // 
             // ConsultarPaciente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 515);
+            this.ClientSize = new System.Drawing.Size(1017, 634);
+            this.Controls.Add(this.CB_Nombre);
             this.Controls.Add(this.TXT_FiltroNombre);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.DGV_Pacientes);
@@ -149,6 +176,7 @@
             this.Controls.Add(this.LINK_AgregarPaciente);
             this.Controls.Add(this.LINK_Menu);
             this.Controls.Add(this.LBL_ConsultaDePacientes);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ConsultarPaciente";
             this.Text = "ConsultarPaciente";
             this.Load += new System.EventHandler(this.ConsultarPaciente_Load);
@@ -170,5 +198,6 @@
         private System.Windows.Forms.DataGridView DGV_Pacientes;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox TXT_FiltroNombre;
+        private System.Windows.Forms.ComboBox CB_Nombre;
     }
 }
--- BD_Grupo3_VS/BD_Grupo3_VS/ConsultarPaciente.cs
+++ BD_Grupo3_VS/BD_Grupo3_VS/ConsultarPaciente.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -56,6 +57,7 @@ namespace BD_Grupo3_VS
         private void ConsultarPaciente_Load(object sender, EventArgs e)
         {
             llenarTabla(DGV_Pacientes, null, null);
+            llenarCombobox(CB_Nombre);
         }
 
         private void LINK_AgregarPaciente_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -64,6 +66,43 @@ namespace BD_Grupo3_VS
             agregarP.Show();
             this.Hide();
 
+        }
+
+        private void llenarCombobox(ComboBox combobox)
+        {
+            SqlDataReader datos = paciente.obtenerListaNombresPacientes();
+            if (datos != null)
+            {
+                while (datos.Read())
+                {
+                    combobox.Items.Add(datos.GetValue(0));
+                }
+            }
+            else
+            {
+                combobox.Items.Clear();
+            }
+            combobox.SelectedItem = 0;
+        }
+
+        private void CB_Nombre_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            llenarTabla(DGV_Pacientes,CB_Nombre.Text,null);
+        }
+
+        private void DGV_Pacientes_CurrentCellChanged_1(object sender, EventArgs e)
+        {
+            string nombre = "";
+            if (DGV_Pacientes.SelectedRows.Count != 0)
+            {
+                //int numero = DGV_Pacientes.CurrentCellAddress.Y;
+                //nombre = DGV_Pacientes[1,2].Value.ToString();
+                DataGridViewRow row = DGV_Pacientes.CurrentRow;
+                nombre = row.Cells[0].Value.ToString();
+                TXT_FiltroNombre.Text = nombre;
+                MessageBox.Show(nombre,null);
+            }
+            
         }
     }
 }
