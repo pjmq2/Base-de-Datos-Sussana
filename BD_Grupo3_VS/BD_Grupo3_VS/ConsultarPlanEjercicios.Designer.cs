d6b71f33924fbd794fdea6b6b0eb003750f050df
Pablo Madrigal <pjmq2@hotmail.com>
2017-07-06 17:12:56 -0600

    Modificaciones varias

--- BD_Grupo3_VS/BD_Grupo3_VS/ConsultarPlanEjercicios.Designer.cs
+++ BD_Grupo3_VS/BD_Grupo3_VS/ConsultarPlanEjercicios.Designer.cs
@@ -189,9 +189,9 @@
             this.button1.Location = new System.Drawing.Point(516, 103);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 117);
+            this.button1.Size = new System.Drawing.Size(107, 117);
             this.button1.TabIndex = 51;
-            this.button1.Text = "Buscar Plan";
+            this.button1.Text = "Modificar Plan";
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button1.UseVisualStyleBackColor = true;
             // 
