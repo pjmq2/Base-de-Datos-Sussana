d035dbfbd201cbb6ced6ba50b66d4d57d3681923
Diego Mora <diegomoraj@outlook.com>
2017-07-07 09:07:35 -0600

    Arreglo en la consulta de Tecnicas

--- BD_Grupo3_VS/BD_Grupo3_VS/Tecnica.cs
+++ BD_Grupo3_VS/BD_Grupo3_VS/Tecnica.cs
@@ -18,7 +18,7 @@ namespace BD_Grupo3_VS
 
         public int agregarTecnica(string nombre, int precio, string descripcion)
         {
-            String insertar = "INSERT into Tecnica (Nombre, Precio, Descripcion)" +
+            String insertar = "INSERT into Tecnicas (Nombre, Precio, Descripcion)" +
                  " VALUES ('" + nombre + "'," + precio + ",'" + descripcion + "')";
             return bd.actualizarDatos(insertar);
         }
@@ -34,11 +34,11 @@ namespace BD_Grupo3_VS
 
             if (filtroNombre == null)
             {
-                bd.ejecutarConsultaTabla("SELECT Nombre, Precio FROM Tecnica");
+                bd.ejecutarConsultaTabla("SELECT Nombre, Precio FROM Tecnicas");
             }
             else
             {
-                bd.ejecutarConsultaTabla("SELECT Nombre, Precio FROM Material WHERE Nombre LIKE '%" + filtroNombre + "%' OR DESCRIPCION LIKE '%" + filtroNombre + "%'");
+                bd.ejecutarConsultaTabla("SELECT Nombre, Precio FROM Tecnicas WHERE Nombre LIKE '%" + filtroNombre + "%' OR DESCRIPCION LIKE '%" + filtroNombre + "%'");
             }
 
             return tabla;
